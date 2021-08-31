10137edc7608b1e332f24638ffd12ff1dd30d241
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2021-08-31 17:50:49 -0400

    parte 2
    Inclusão do react-rating-stars-component  para definir as estrelas dos restaurantes.
    Recebendo via props as informações do restaurante.

--- src/components/RestaurantCard/styles.js
+++ src/components/RestaurantCard/styles.js
@@ -15,3 +15,27 @@ export const Restaurant = styled.div `
 
     }
 `;
+
+export const RestaurantInfo= styled.div`
+    display: flex;
+    flex-direction: column;
+
+`;
+
+export const Title = styled.span`
+    font-family: ${(props)=> props.theme.fonts.regular };
+    color: ${(props)=> props.theme.colors.text};
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 29px;
+    margin-bottom: 10px;
+`;
+
+export const Address = styled.span `
+    font-family: ${(props)=> props.theme.fonts.regular };
+    color: ${(props)=> props.theme.colors.text};
+    font-size: 16px;
+    line-height: 19px;
+    margin-bottom: 10px;
+    margin-top: 10px;
+`;
