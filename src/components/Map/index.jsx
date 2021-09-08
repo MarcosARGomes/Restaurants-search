5229a6f81d9e1d1a83bee1095db72351e81e9c98
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2021-09-08 17:26:25 -0400

    Busca
    Iniciando a implementação da busca.

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
 
 export const MapContainer = (props) => {
+    const [map, setMap] = useState(null);
     const {google} = props;
 
+    function searchNearby(map, center){
+        const service = new google.maps.places.PlacesService(map); 
+
+        const request = {
+            location: center,
+            radius:'20000', /*pega os restaurantes em ate 20 mil metros do local do usuário */
+            type: ['restaurant'] /*pegar somente os lugares cadastrados como type restaurantes*/
+        };
+
+        service.nearbySearch(request, (results, status) => {
+            if(status === google.maps.places.PlacesService.OK){
+                console.log('restaurants>>>', results);
+            }
+        });
+    }
+
+    function onMapReady(_, map){
+        setMap(map);
+        searchNearby(map, map.center);
+    }
+
     return(
-        <Map google={google} centerAroundCurrentLocation />
+        <Map google={google} centerAroundCurrentLocation onReady={onMapReady} onRecenter={onMapReady} /> /*Responsavel por buscar a localicação atual do usuário */
 
         
     );
