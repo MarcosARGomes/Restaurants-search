ad89ae44ec4325db1cfacf303219301ec54df20d
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2021-09-11 10:20:36 -0300

    implementação do Marker

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
+import {useDispatch} from 'react-redux';
 import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
 
+import { setRestaurant, setRestaurants} from '../../redux/modules/restaurants';
+
 export const MapContainer = (props) => {
+    const dispatch = useDispatch();
     const [map, setMap] = useState(null);
     const {google, query} = props;
 
@@ -40,6 +44,7 @@ export const MapContainer = (props) => {
         service.nearbySearch(request, (results, status) => {
             if(status === google.maps.places.PlacesServiceStatus.OK){
                 console.log('restaurants>>>', results);
+                dispatch(setRestaurants(results));
             }
         });
     }
